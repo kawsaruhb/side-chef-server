373482973ca94af85dfe895971fe2c7681842e81
kawsaruhb <kawsaruhb@gmail.com>
2023-05-26 00:45:41 +0600

    json data added

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
-// const express = require('express')
-// const app = express()
-// const cors = require('cors')
-// const port = process.env.PORT || 5000;
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const port = process.env.PORT || 5000;
 
-// const chefs = require('./data/chef.json')
+const chefs = require('./data/chef.json')
 
-// app.use(cors());
+app.use(cors());
 
-// app.get('/', (req, res) => {
-//   res.send('Server Running....!')
-// })
+app.get('/', (req, res) => {
+  res.send('Server Running....!')
+})
 
-// app.get('/chefs', (req, res) => {
-//     res.send(chefs);
-// })
+app.get('/chefs', (req, res) => {
+    res.send(chefs);
+})
 
-// app.get('/chefs/:id', (req, res) =>{
-//     const id = req.params.id;
-//     const selectedChef = chefs.find(n => n._id === id);
-//     res.send(selectedChef);
-// })
+app.get('/chefs/:id', (req, res) =>{
+    const id = req.params.id;
+    const selectedChef = chefs.find(n => n._id === id);
+    res.send(selectedChef);
+})
 
-// app.listen(port, () => {
-//   console.log(`Example app listening on port ${port}`)
-// })
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
